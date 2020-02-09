64590ea1939c6a97bc307552215123e5b712fee7
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-02-10 00:46:17 +1100

    Moved operator data to operators subdir

--- arknights/pipelines/json.py
+++ arknights/pipelines/json.py
@@ -4,7 +4,7 @@ import json
 class JsonPipeline(object):
     @staticmethod
     def process_item(item, _):
-        filename = 'data/' + item['slug'] + '.json'
+        filename = 'data/operators/' + item['slug'] + '.json'
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(item, f)
         return item
