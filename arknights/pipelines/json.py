561a806351df0f4d7ea3b193ddaf555893cbb645
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-02-10 10:43:43 +1100

    Add \n to end of json output
    This matches the output to the json formatting output in the
    pre-commit hook.

--- arknights/pipelines/json.py
+++ arknights/pipelines/json.py
@@ -9,4 +9,5 @@ class JsonPipeline(object):
         )
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(item, f, indent=2, sort_keys=True)
+            f.write("\n")
         return item
