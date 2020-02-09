b6bd705cf5e265aa4ce7077e4d5e8a67e1b2d03f
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-02-10 01:44:32 +1100

    pre-commit run --all-files

--- arknights/pipelines/json.py
+++ arknights/pipelines/json.py
@@ -4,7 +4,7 @@ import json
 class JsonPipeline(object):
     @staticmethod
     def process_item(item, _):
-        filename = 'data/operators/' + item['slug'] + '.json'
-        with open(filename, 'w', encoding='utf-8') as f:
+        filename = "data/operators/" + item["slug"] + ".json"
+        with open(filename, "w", encoding="utf-8") as f:
             json.dump(item, f)
         return item
--- arknights/spiders/basic.py
+++ arknights/spiders/basic.py
@@ -4,24 +4,24 @@ from scrapy import Spider
 
 
 class BasicSpider(Spider):
-    name = 'operators'
-    allowed_domains = ['en.rhinelab.org']
-    start_urls = ['http://en.rhinelab.org/List_of_Operators_by_Image']
+    name = "operators"
+    allowed_domains = ["en.rhinelab.org"]
+    start_urls = ["http://en.rhinelab.org/List_of_Operators_by_Image"]
 
     def parse(self, response):
-        for a in response.css('.charbadge-image > a'):
+        for a in response.css(".charbadge-image > a"):
             yield response.follow(a, self.parse_operator)
 
     @staticmethod
     def parse_operator(response):
         def extract(query):
-            return response.css(query).get(default='').strip()
+            return response.css(query).get(default="").strip()
 
-        name = extract('.character-name::text')
+        name = extract(".character-name::text")
         if not name:
-            name = extract('h1::text')
+            name = extract("h1::text")
         yield {
-            'slug': slugify(name),
-            'name': name,
-            'stars': len(response.css('.character-stars span')),
+            "slug": slugify(name),
+            "name": name,
+            "stars": len(response.css(".character-stars span")),
         }
